d32ff9ff0eacf603d9691a2b6808298ec2da091e
Alex Peck <alexpeck@microsoft.com>
2022-02-11 22:36:37 -0800

    sdk style

--- Base64/Infra/MemoryStreamFactory.cs
+++ Base64/Infra/MemoryStreamFactory.cs
@@ -10,7 +10,6 @@ namespace Base64
 	using System.Collections.Generic;
 	using System.IO;
 	using System.Linq;
-	using System.Runtime.Remoting.Messaging;
 	using System.Text;
 	using System.Threading.Tasks;
 	using System.Web;
